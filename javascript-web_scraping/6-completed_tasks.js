77f07a37a60c3e770502f4734c070cec09176a28
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-16 08:06:45 -0400

    tasks

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,25 +3,33 @@
 const request = require('request');
 const apiUrl = process.argv[2];
 
-request(apiUrl, { json: true }, (error, response, todos) => {
+function handleResponse(error, response, todos) {
   if (error) {
-    console.error('Error:', error);
+    console.error('Error fetching data:', error);
     return;
   }
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data, Status Code:', response.statusCode);
+    return;
+  }
+  const counts = countCompletedTasks(todos);
+  printCounts(counts);
+}
 
+function countCompletedTasks(todos) {
   const completedTasks = {};
-
   todos.forEach(todo => {
     if (todo.completed) {
-      if (completedTasks[todo.userId]) {
-        completedTasks[todo.userId] += 1;
-      } else {
-        completedTasks[todo.userId] = 1;
-      }
+      completedTasks[todo.userId] = (completedTasks[todo.userId] || 0) + 1;
     }
   });
+  return completedTasks;
+}
 
-  for (const userId in completedTasks) {
+function printCounts(completedTasks) {
+  Object.keys(completedTasks).forEach(userId => {
     console.log(`'${userId}': ${completedTasks[userId]}`);
-  }
-});
+  });
+}
+
+request(apiUrl, { json: true }, handleResponse);
