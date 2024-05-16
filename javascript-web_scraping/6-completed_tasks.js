d0a6ae1dfd851977aea44e9cf0bb1728c736fe53
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-16 08:14:48 -0400

    tasks

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,34 +2,34 @@
 const request = require('request');
 
 if (process.argv.length < 3) {
-    console.error('Usage: ./6-completed_tasks.js <API URL>');
-    process.exit(1);
+  console.error('Usage: ./6-completed_tasks.js <API URL>');
+  process.exit(1);
 }
 
 const apiUrl = process.argv[2];
 
 request(apiUrl, function (error, response, body) {
-    if (error) {
-        console.error(error);
-        return;
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Failed to get data from the API');
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach(function (todo) {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId] += 1;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
     }
-    if (response.statusCode !== 200) {
-        console.error('Failed to get data from the API');
-        return;
-    }
+  });
 
-    const todos = JSON.parse(body);
-    const completedTasks = {};
-
-    todos.forEach(function (todo) {
-        if (todo.completed) {
-            if (completedTasks[todo.userId]) {
-                completedTasks[todo.userId] += 1;
-            } else {
-                completedTasks[todo.userId] = 1;
-            }
-        }
-    });
-
-    console.log(completedTasks);
+  console.log(completedTasks);
 });
