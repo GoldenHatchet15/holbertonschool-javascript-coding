b12f968ff6b5f9bfd8b4256b6a9a7287ef4905cb
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-16 07:30:23 -0400

    tasks

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,15 +3,14 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (error, data) => {
-  if (error) {
-    console.log(`{ Error: ${error.code}: no such file or directory, open '${filePath}'
+try {
+  const data = fs.readFileSync(filePath, 'utf8');
+  console.log(data);
+} catch (error) {
+  console.error(`{ Error: ${error.code}: no such file or directory, open '${filePath}'
     at Error (native)
     errno: ${error.errno},
     code: '${error.code}',
     syscall: '${error.syscall}',
     path: '${filePath}' }`);
-  } else {
-    console.log(data);
-  }
-});
+}
