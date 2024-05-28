b304b6daacfe91fb37712fb34feeb29918cbc045
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 04:40:30 -0400

    tasks

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,30 +1,10 @@
-// 4-http.js
-
 const http = require('http');
 
-// Create HTTP server
 const app = http.createServer((req, res) => {
-  // Set response headers
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send response body
-  res.end('Hello Holberton School!\n');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(1245);
 
-// Graceful shutdown
-process.on('SIGINT', () => {
-  console.log('Received SIGINT. Shutting down server...');
-  app.close(() => {
-    console.log('Server closed.');
-    process.exit(0);
-  });
-});
-
-// Export the app
 module.exports = app;
