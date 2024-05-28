9d99123242cf4be600f339a066b3f6b4dc9571d8
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 03:17:25 -0400

    tasks

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
 // 1-stdin.js
+
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-  const name = input.trim();
+  const name = input.toString().trim(); // Convert input to string and trim whitespace
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
     process.stdout.write('This important software is now closing\n');
