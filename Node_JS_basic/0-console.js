78abff0423723ec6a95d5bb3dd50586fb5d4439a
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 01:00:42 -0400

    tasks

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
+/**
+ * Prints a message to the standard output.
+ * @param {string} message - The message to be printed.
+ */
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
