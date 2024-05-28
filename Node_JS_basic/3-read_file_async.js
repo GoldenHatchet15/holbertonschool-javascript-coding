3e9b8e94cfc8121ae50a5575abd214572107ebd4
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 04:10:05 -0400

    tasks

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+// 3-read_file_async.js
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const rows = data.trim().split('\n');
+        let CSCount = 0;
+        let SWECount = 0;
+        const csList = [];
+        const sweList = [];
+
+        for (let i = 1; i < rows.length; i++) {
+          const row = rows[i];
+          const [firstName, lastName, age, field] = row.split(',');
+
+          if (field.trim() === 'CS') {
+            CSCount++;
+            csList.push(firstName);
+          } else if (field.trim() === 'SWE') {
+            SWECount++;
+            sweList.push(firstName);
+          }
+        }
+
+        console.log(`Number of students: ${CSCount + SWECount}`);
+        console.log(`Number of students in CS: ${CSCount}. List: ${csList.join(', ')}`);
+        console.log(`Number of students in SWE: ${SWECount}. List: ${sweList.join(', ')}`);
+
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
