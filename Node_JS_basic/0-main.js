c06a6ebe6facfa19c1e860559bd77fb01746bc93
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 00:57:33 -0400

    tasks

---
+++ Node_JS_basic/0-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// Import the displayMessage function
+const displayMessage = require('./0-console');
+
+// Call the displayMessage function with a test message
+displayMessage('Hello NodeJS!');
