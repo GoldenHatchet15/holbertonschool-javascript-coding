d3da63068f8a154d1c5fbab91e26492b45e0429c
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 00:45:07 -0400

    tasks

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,8 +1,7 @@
 // Define the function to display a message
 function displayMessage(message) {
-    console.log(message);
-  }
-  
-  // Export the displayMessage function for use in other files
-  module.exports = displayMessage;
-  
+  console.log(message);
+}
+
+// Export the displayMessage function for use in other files
+module.exports = displayMessage;
