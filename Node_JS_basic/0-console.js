7d3d183793a7a84adc0ed828eb4f5cbad4fa3835
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-28 01:06:05 -0400

    tasks

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,10 @@
+/**
+ * Function to print a message to the console
+ * @param {string} message - The message to print
+ */
 const displayMessage = (message) => {
   console.log(message);
 };
 
-
+// Export the displayMessage function
 module.exports = displayMessage;
