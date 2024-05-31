6dac4e1415effa241860ec5a3483f819fec9d2b6
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-31 19:56:06 -0400

    task 3 fix

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,42 +1,32 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const rows = data.trim().split('\n');
-        let CSCount = 0;
-        let SWECount = 0;
-        const csList = [];
-        const sweList = [];
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n');
+      const students = lines.filter((line) => line).map((line) => line.split(','));
 
-        for (let i = 1; i < rows.length; i++) {
-          const row = rows[i].trim();
-          if (!row) {
-            continue;
-          }
+      const num_students = students.length ? students.length - 1 : 0;
+      console.log(`Number of students: ${num_students}`);
 
-          const [firstName, , , field] = row.split(',');
-
-          if (field.trim() === 'CS') {
-            CSCount += 1;
-            csList.push(firstName);
-          } else if (field.trim() === 'SWE') {
-            SWECount += 1;
-            sweList.push(firstName);
-          }
+      const fields = {};
+      for (const student of students) {
+        const field = student[3];
+        if (!fields[field]) {
+          fields[field] = [];
         }
-
-        console.log(`Number of students: ${CSCount + SWECount}`);
-        console.log(`Number of students in CS: ${CSCount}. List: ${csList.join(', ')}`);
-        console.log(`Number of students in SWE: ${SWECount}. List: ${sweList.join(', ')}`);
-
-        resolve();
+        fields[field].push(student[0]);
       }
+
+        delete fields.field;
+
+      for (const field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
     });
-  });
 }
 
 module.exports = countStudents;
